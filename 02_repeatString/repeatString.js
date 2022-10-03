1b6e195a9083c1da3e75f2dd2f258ad6f220da33
krmanski <choromanski117@gmail.com>
2022-10-03 19:25:55 -0400

    Complete ex 02
    This one was tougher but I got it. I was just tired and makeing
    stupid errors. I was not declaring variables, sending args to the
    wrong part of for-loop syntax, and being cavalier where I wrote
    my "return" statements. Time for a break.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+// const repeatString = function(text, number) {
+//     let repeatedString = '';
+//     repetant = text
+//     for (let i = number; i < 3; i++){
+//         repeatedString += repetant;
+//         return repeatedString;
+//     }
+// };
 
+
+
+function repeatString(text, number) {
+    if (number < 0 ) return "ERROR";
+    let repeatedString = '';
+    let repetant = text
+    for (let i = 0; i < number; i++){
+        repeatedString += repetant;
+        
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
