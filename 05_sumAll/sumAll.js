4c7f4198bc2baf96f02b34fbd850e90f76990ef4
krmanski <choromanski117@gmail.com>
2022-10-04 15:28:05 -0400

    Complete js ex05 boilerplate
    Currently tyring to write function that takes all numbers between
    two numbers and returns a sum of them all. The function now
    successully returns an error if the args are negative or not an
    integer. It also does not matter which number you put first.
    Everything working well.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(numberA, numberB) {
+    if ((numberA < 0) || (numberB < 0)) return "ERROR";
+    if (!(Number.isInteger(numberA)) || !(Number.isInteger(numberB))) return "ERROR";
+    const bigNum = Math.max(numberA, numberB);
+    const smallNum = Math.min(numberA, numberB);
+    console.log(`the small number is ${smallNum} and the big number is ${bigNum}`)
 };
+
+
+
+
+// PASS THESE TESTSS
+// 'works with large numbers'
+// DONE works with larger number first' 
+// DONE returns ERROR with negative numbers'
+// DONE returns ERROR with non-number parameters'
 
 // Do not edit below this line
 module.exports = sumAll;
