a7db9167be966ec7ab02eb4653bb34115b3607e4
krmanski <choromanski117@gmail.com>
2022-10-04 15:36:33 -0400

    Complete js ex05
    Successfully adds all numbers between 2 integer arguments passed to
    the function. Working beautifully.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,20 @@
 const sumAll = function(numberA, numberB) {
+    //ensures that the arguments are positive and integers. 
     if ((numberA < 0) || (numberB < 0)) return "ERROR";
     if (!(Number.isInteger(numberA)) || !(Number.isInteger(numberB))) return "ERROR";
+    //makes sure that it doesn't matter weather the bigger number is the first or second arg
     const bigNum = Math.max(numberA, numberB);
     const smallNum = Math.min(numberA, numberB);
-    console.log(`the small number is ${smallNum} and the big number is ${bigNum}`)
+    let sum = 0;
+    
+    for (i = smallNum; i < bigNum; i++){
+        sum += i;
+        console.log(`adding ${i} to ${sum}`)
+    }
+    return sum;
+
+    //test line
+    // console.log(`the small number is ${smallNum} and the big number is ${bigNum}`);
 };
 
 
