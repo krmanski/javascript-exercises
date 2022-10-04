8a11b1dd3f1068ee34494acf010a583a9f2e26ae
krmanski <choromanski117@gmail.com>
2022-10-04 14:35:36 -0400

    Passes all tests now

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -9,20 +9,20 @@ const removeFromArray = function(array, toRemove) {
 
 
 };
-let array1 = [1, 2, 3, 4, 5, 6, 7, 8];
-
-    //get an argument specifically from here
-    (arguments[0]);
-    (arguments[2]);
-    //get the last argument passed into the function, no matter how many there were. 
-    (arguments[(arguments.length -1)]);
 
 
-let filteredArray = array1.filter(x => x > 3);
-//Take array and iterate through each item of it
-    //See if each item of the array is === to the args passed in
-    //make sure function can remove multiple args
-console.log(filteredArray);
+//     //get an argument specifically from here
+//     (arguments[0]);
+//     (arguments[2]);
+//     //get the last argument passed into the function, no matter how many there were. 
+//     (arguments[(arguments.length -1)]);
+
+
+// let filteredArray = array1.filter(x => x > 3);
+// //Take array and iterate through each item of it
+//     //See if each item of the array is === to the args passed in
+//     //make sure function can remove multiple args
+// console.log(filteredArray);
 
 // Do not edit below this line
 module.exports = removeFromArray;
