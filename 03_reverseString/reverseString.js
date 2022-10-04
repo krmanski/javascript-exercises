58fb025bb598b130d35811936f664aca29072ff8
krmanski <choromanski117@gmail.com>
2022-10-04 11:27:45 -0400

    Complete js ex03
    Program successfully reverses a string. I used charAt(self.length-1)
    and slice(0 -1) to pull out the last character of a string and
    append it to a new string each loop while also shortening the
    old string to one less letter from the end each iteration.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    //First we need to ensure the user passed in something and that it is a string
+    if (stringToReverse == undefined) return "ERROR";
+    const rawString = String(stringToReverse);
+    let shrinkingString = rawString;
+    let reversed = '';
 
+    for (let i = 0; i < rawString.length; i++){
+        let nextCharacter = shrinkingString.charAt(shrinkingString.length - 1);
+        reversed += nextCharacter;
+        //the old string now must be one letter shorter from the end.
+        shrinkingString = shrinkingString.slice(0, -1);
+        //temporary readout to test code
+        // console.log(reversed);
+        // console.log(nextCharacter);
+        // console.log(shrinkingString);
+    }
+    return reversed;
+    //remove last element of a string and append it as the next element in a blank string
 };
 
 // Do not edit below this line
