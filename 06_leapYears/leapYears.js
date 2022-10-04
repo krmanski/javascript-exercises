771ac621148bb8402f4c96425625f9d5178ec204
krmanski <choromanski117@gmail.com>
2022-10-04 16:08:08 -0400

    Complete js ex05
    Passes all tests and successfully evaluates if something is a leap
    year or not.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
+    if (year % 4 !== 0) return false;
+    if ((year % 100 == 0) && (year % 400 != 0)) return false;
+    else return true;
 };
+
+
+
+//to test
+// 'works with non century years'
+// 'works with ridiculously futuristic non century years'
+// 'works with century years'
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
