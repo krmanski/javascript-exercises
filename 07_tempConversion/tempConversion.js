ee13fce87e4ff2109396e4a76326511fcdb6d5e7
krmanski <choromanski117@gmail.com>
2022-10-04 16:29:09 -0400

    Finish Fahrenheit to Celsius
    Currently making 2 funcitons. One that converts F to C and one that
    converts C to F. F to C currently works great and meets requirements.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,28 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+  //makes sure the user provided an integer
+  if (!Number.isInteger(tempF)) return "ERROR";
+  let answer = (tempF - 32) * (5 / 9);
+  //rounds the number to one decimal place for readability. 
+  answer = Math.round(answer * 10) / 10;
 
+  //test line
+  console.log(answer);
 };
+
+//PASS THESE TESTS
+// 'math works'
+// 'rounds to 1 decimal'
+// 'works with negatives'
 
 const ctof = function() {
 
 };
+
+
+//PASS THESE TESTS
+// 'math works'
+// 'rounds to 1 decimal'
+// 'works with negatives'
 
 // Do not edit below this line
 module.exports = {
