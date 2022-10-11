53970304934a99a70acaf799fdcf0b8e6c2ccff4
krmanski <choromanski117@gmail.com>
2022-10-11 19:54:28 -0400

    Reverses string
    Function successfully takes a string, converts it to an array,
    uses push/pop to reverse it, then turns it back into a string.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,37 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //make the arg an array if it is not already
+    if ((typeof string) != 'string')  return "ERROR. Please only use a string."  
+    const transArray = Array.from(string);
+    
+    const newArray = []
+    //take each item from the rear of the array and add to new array
+    for (i = transArray.length; i > 0; i--){
+        let letter = transArray.pop();
+        newArray.push(letter)
+        // console.log(letter, transArray, newArray);
+        
+
+    }
+    let oldArray = Array.from(string);
+    console.log(oldArray, newArray);
+    let reversedString = newArray.join("");
+    console.log(string, reversedString);
+}
+
+
+
+
+
+
+
+
+//evaluate the old and new array for equivilancy
+//write an if-statement for true/false
+
+
+
+
+
 
 };
 
