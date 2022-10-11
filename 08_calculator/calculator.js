f9f2da701916cb25b57226b621b3ca387b65eb1f
krmanski <choromanski117@gmail.com>
2022-10-11 19:06:44 -0400

    Complete js ex08
    Calculator base functionality built to compute basic functions
    addition, subtraction, multiplication, exponentiation, and
    factorialization. No division yet.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,7 +8,7 @@ const subtract = function(minuend, subtrahend) {
 
 const sum = function(array) {
 	let sumTotal = array.reduce((total, arrayItem) =>{
-    console.log(total, arrayItem);
+    
     return total + arrayItem;
 
   }, 0);
@@ -16,16 +16,36 @@ const sum = function(array) {
   return sumTotal;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	let sumTotal = array.reduce((total, arrayItem) =>{
+    //iterrates over every item in the array and multiplies it by a running total.
+    return total * arrayItem;
+    //the initial value of this reduce() function must actually start at 1 instead of 0 because we are multiplying. Else everything would be 0. 
+  }, 1);
 
+  return sumTotal;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  //makes an array with as many slots as the power. 
+  //then fill puts the base number in each slot. So 4^3 would make a [4,4,4] array.
+  let arrayToX = Array(exponent).fill(base);
+  let product = multiply(arrayToX);
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+	if (factor < 0) return "ERROR";
+  if (factor === 0) return 1;
+  let arrayToX = []
+  for (let i = factor; i > 0; i--){
+    console.log(i);
+    arrayToX.push(i);
+    console.log(arrayToX);
+  }
+  let product = multiply(arrayToX);
+  return product;  
+
 };
 
 // Do not edit below this line
