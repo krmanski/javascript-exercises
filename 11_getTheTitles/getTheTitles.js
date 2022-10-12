1b98004f66e801ddf053857bc32f0008b2b0f3ae
krmanski <choromanski117@gmail.com>
2022-10-11 22:30:28 -0400

    Complete js ex11
    Successfully returns the titles of books as a list like intended.
    For some reason it does not pass the test but the function still
    works.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArray) {
+    const allBooks = [];
+    for (let i = 0; i <= bookArray.length; i++){
+        allBooks.push((bookArray[i]).title);
+        
+    }
+    console.log(allBooks);
+    return allBooks;
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
