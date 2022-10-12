3a689d261c63d9a638ffbf440a4ec9ad534e3596
krmanski <choromanski117@gmail.com>
2022-10-11 20:56:25 -0400

    Passes all but one test
    Unsure why but some strings are getting framshift mutated. All
    other functionalities successful.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,21 +1,21 @@
+const punc = /[!,-./:;<=>?@[\]^_`{|}~]/g;
 const palindromes = function (string) {
     //make the arg an array if it is not already
     if ((typeof string) != 'string')  return "ERROR. Please only use a string."  
+    const upperCaseString = string.toUpperCase();
+    const processedString = upperCaseString.replace(punc,"");
     
-    const processedString = string.toUpperCase();
-    console.log(processedString);
     const transArray = Array.from(processedString);
     
     const newArray = []
     //take each item from the rear of the array and add to new array
     for (i = transArray.length; i > 0; i--){
         let letter = transArray.pop();
-        newArray.push(letter)
+        newArray.push(letter);
         // console.log(letter, transArray, newArray);
-        
-
     }
     let reversedString = newArray.join("");
+    
     if (processedString == reversedString){
         return true;
     } else if (processedString != reversedString){
@@ -30,16 +30,6 @@ const palindromes = function (string) {
 
 
 
-
-//evaluate the old and new array for equivilancy
-//write an if-statement for true/false
-
-
-
-
-
-
-};
 
 // Do not edit below this line
 module.exports = palindromes;
