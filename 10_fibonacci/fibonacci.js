b914a02e9328e942c3bddb4ae5cb44e44d91bc4f
krmanski <choromanski117@gmail.com>
2022-10-11 21:54:32 -0400

    Returns last n of fibonacci
    Successfully takes a string or integer input and
    returns the correct number of the fibonacci sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,41 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let n = parseInt(number);
+    
+    if (n < 0) return "OOPS";
+
+    let fib = [0];
+    
+    for(let i = 1; i <= n; i++){
+        if (fib.length < 2){
+            fib.push(1);
+            console.log(fib);
+        } else {
+            let addend1 = fib[fib.length -1];
+            let addend2 = fib[fib.length -2];
+            let step = addend1 + addend2;
+            console.log(`${addend1} + ${addend2}`);
+            fib.push(step);
+            console.log(fib);
+        }
+        console.log(fib);
+        console.log(fib[fib.length -1]);
+
+         
+        // let y = x[x.length -2]
+    }
+
+    return fib[n];
+
 
 };
+
+
+
+//return that n of fibonacci
+
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
